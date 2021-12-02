3f3060b14a105b6802258becfef497c7387c72a2
JendaKolda <email@jendakolda.cz>
2021-12-02 14:41:17 +0100

    day 2

---
+++ Day2/day2.py
@@ -0,0 +1,33 @@
+forward = 0
+depth = 0
+# Part A:
+with open('day2_input.txt', 'r', encoding='utf-8') as movement:
+    for line in movement:
+        command, value = line.split()
+        value = int(value)
+        match command:
+            case 'up':
+                depth -= value
+            case 'down':
+                depth += value
+            case 'forward':
+                forward += value
+print('Part A:', forward * depth)
+
+# Part A:
+forward = 0
+depth = 0
+aim = 0
+with open('day2_input.txt', 'r', encoding='utf-8') as movement:
+    for line in movement:
+        command, value = line.split()
+        value = int(value)
+        match command:
+            case 'up':
+                aim -= value
+            case 'down':
+                aim += value
+            case 'forward':
+                forward += value
+                depth += aim * value
+print('Part B:', forward * depth)
