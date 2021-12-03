ade8b5bc13e5bec16258799b6930f495f5da9b70
JendaKolda <email@jendakolda.cz>
2021-12-03 16:56:24 +0100

    day 3 unfinished 2

--- Day3/day3.py
+++ Day3/day3.py
@@ -3,7 +3,7 @@ from collections import deque
 
 def get_rating(binaries, crit):
     for position in range(len(binaries[0])):
-        criterion = most_least_common(binaries, position, function=crit)
+        criterion = most_least_single(binaries, position, function=crit)
         filtered = list(filter(lambda x: x[position] == criterion, binaries))
         binaries = filtered[1:]
         if len(binaries) == 1:
@@ -18,29 +18,29 @@ def binlist_to_int(binlist):
     return int(''.join(binlist), 2)
 
 
+def most_least_single(binaries, position, function):
+    summa = 0
+    for i in range(len(binaries)):
+        summa += binaries[i][position]
+
+
+        match function:
+            case 'most':
+                return binlist_to_int([str(1) if i > len(binaries) / 2 else str(0) for i in summas])
+            case 'least':
+                return binlist_to_int([str(1) if i <= len(binaries) / 2 else str(0) for i in summas])
+
+
 def most_least_common(binaries, positions=None, function='both'):
-    if positions:
-        summas = 0
-        for i in len(binaries):
-            summas += binaries[i][positions]
-        summas = list(summas)
-        print(summas, 'summas')
-    else:
-        summas = list()
-        for j in range(len(binaries[0])):
-            counter = 0
-            for i in range(len(binaries)):
-                counter += binaries[i][j]
-            summas.append(counter)
-    match function:
-        case 'most':
-            return binlist_to_int([str(1) if i > len(binaries) / 2 else str(0) for i in summas])
-        case 'least':
-            return binlist_to_int([str(1) if i <= len(binaries) / 2 else str(0) for i in summas])
-        case 'both':
-            most = binlist_to_int([str(1) if i > len(binaries) / 2 else str(0) for i in summas])
-            least = binlist_to_int([str(1) if i <= len(binaries) / 2 else str(0) for i in summas])
-            return most, least
+    summas = list()
+    for j in range(len(binaries[0])):
+        counter = 0
+        for i in range(len(binaries)):
+            counter += binaries[i][j]
+        summas.append(counter)
+        most = binlist_to_int([str(1) if i > len(binaries) / 2 else str(0) for i in summas])
+        least = binlist_to_int([str(1) if i <= len(binaries) / 2 else str(0) for i in summas])
+        return most, least
 
 
 with open('day3_input.txt', 'r', encoding='utf-8') as readings:
@@ -54,7 +54,7 @@ print('Part A:', gamma * epsilon)
 
 # Part B:
 # oxygen == most
-print('Part B:', get_rating(byte_list, 'most'))
+# print('Part B:', get_rating(byte_list, 'most'))
 
 # least_bit = most_least_common(byte_list, 1, function='least')
 # filtered_least = list(filter(lambda x: x[0] == least_bit, byte_list))
